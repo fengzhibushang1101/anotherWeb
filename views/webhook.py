558e3022ea90c764f78c0742ecbd300cb7ce4318
yitian <fengzhibushang@163.com>
2018-03-27 15:02:56 +0800

    add email tip in webhookhandler

--- views/webhook.py
+++ views/webhook.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+ @Time    : 2018/3/25 0025 上午 11:52
+ @Author  : Administrator
+ @Software: PyCharm
+ @Description:
+"""
+
+
 from views.base import BaseHandler
 
 
@@ -6,5 +16,5 @@ class WebHookHandler(BaseHandler):
     def post(self, *args, **kwargs):
         import subprocess
         cwd = "/root/src/anotherWeb"
-        subprocess.Popen("git pull origin master; supervisorctl restart all", cwd=cwd, shell=True)
+        subprocess.Popen("git pull origin master; supervisorctl restart all; echo '更新成功!!!' | mail -s '项目更新成功!!!' fengzhibushang@163.com", cwd=cwd, shell=True)
 
