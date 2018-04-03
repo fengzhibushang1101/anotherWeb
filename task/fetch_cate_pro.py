e0577b103db84bf0ea47f52c5b77e5750df18d8e
yitian <fengzhibushang@163.com>
2018-04-03 11:43:12 +0800

    add page_count

--- task/fetch_cate_pro.py
+++ task/fetch_cate_pro.py
@@ -22,7 +22,7 @@ import ujson as json
 def fetch_cate_pro(token, cate_id, off=0):
     url = 'https://api.joom.com/1.1/search/products?language=en-US&currency=USD'
     params = {
-        'count': 50,
+        'count': 200,
         'pageToken': 'off:%s' % off,
         'filters': [{
             'id': 'categoryId',
@@ -34,7 +34,7 @@ def fetch_cate_pro(token, cate_id, off=0):
             }
         }]
     }
-    logger.info(u"正在抓取分类%s下第%s-%s个产品" % (cate_id, off, off + 50))
+    logger.info(u"正在抓取分类%s下第%s-%s个产品" % (cate_id, off, off + 200))
     res = requests.post(url, data=json.dumps(params), headers={
         "authorization": token,
         "content-type": 'application/json'
@@ -52,7 +52,7 @@ def fetch_cate_pro(token, cate_id, off=0):
         for item in items:
             logger.info(u'产品id为%s' % item["id"])
             fetch_review.delay(item["id"], token)
-        with futures.ThreadPoolExecutor(max_workers=16) as executor:
+        with futures.ThreadPoolExecutor(max_workers=32) as executor:
             future_to_user = {
                 executor.submit(fetch_pro, tag=item["id"], token=token): item["id"] for item in items
             }
@@ -62,6 +62,6 @@ def fetch_cate_pro(token, cate_id, off=0):
                     rp = future.result()
                 except Exception as exc:
                     logger.error("%s generated an exception: %s" % (rev_pro, exc))
-        fetch_cate_pro.delay(token, cate_id, off+50)
+        fetch_cate_pro.delay(token, cate_id, off+200)
 
 
