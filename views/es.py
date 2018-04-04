c8c858fd84580a7e4e426c1caa1f87ed5375c157
yitian <fengzhibushang@163.com>
2018-04-04 22:17:05 +0800

    es init

---
+++ views/es.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+ @Time    : 2018/4/4 16:22
+ @Author  : jyq
+ @Software: PyCharm
+ @Description: 
+"""
+from views.base import BaseHandler
+
+
+class EsHandler(BaseHandler):
+
+
+    def get(self, *args, **kwargs):
+        pass
+
+    def post(self, *args, **kwargs):
+        pass
+
