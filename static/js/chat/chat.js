092bcd735a53c36affe7808baaacbe7e5f0eef1d
Tian Yi <fengzhibushang@163.com>
2018-04-07 20:41:14 +0800

    add close mess

--- static/js/chat/chat.js
+++ static/js/chat/chat.js
@@ -5,6 +5,18 @@ $(function () {
     };
     ws.onmessage = function (evt) {
         var data = JSON.parse(evt.data);
+        writeMessage(data);
+
+    };
+    ws.onclose = function(ev) {
+        console.log(ev);
+        writeMessage({
+            type: 'self',
+            message: "链接断开, 请刷新页面重新链接"
+        })
+    };
+
+    function writeMessage(data) {
         if (data["type"] === "sys") {
             $('#chat-content').append("<p style='width: 100%; text-align:center;'><span class='sys-mess'>" + data['message'] + "</span></p>");
         } else if (data["type"] === "self") {
@@ -12,8 +24,7 @@ $(function () {
         } else {
              $('#chat-content').append("<p class='my-mess'>" + data['name'] + ": <br>" +"<span style='color: red'>" + data['message'] + "</span>" + "</p>");
         }
-
-    };
+    }
 
     $("#send-message").click(function() {
         var content = $("#chat-text"),
