93071a11326bc6300ccee13c164280831bc99aac
jiyueqing <jiyueqing@bytedance.com>
2018-08-24 09:54:30 +0800

    feature: 获取剑三信息接口增加返回上次更新事件
    Change-Id: I31508bf7e45cf663b30b8a39f9abfe7154cc429a

--- lib/controls/jx3info.py
+++ lib/controls/jx3info.py
@@ -12,30 +12,31 @@ from redis import RedisError, WatchError
 from lib.nosql.redis_util import redis_conn as redis
 from lib.sql.jx3_daily_record import Jx3DailyRecord
 from lib.sql.session import sessionCM
-from lib.utils.time_utils import Timer
+import lib.utils.time_utils as Timer
 from task.mail import send_to_master
 
 
 class Jx3Info(object):
     BATTLEGROUND_ORDER = [["云湖天池", 10], ["三国古战场", 15], ["浮香丘", 15], ["坑爹之路", 25], ["神农洇", 15], ["九宫棋谷", 25]]
     REDIS_KEY = "jx3_current"
-    TIME_KEY = "jx3_update_time"
+    LAST_UPDATE_TIME = "jx3_update_time"
 
     @classmethod
     def get_info(cls):
         current = redis.get(cls.REDIS_KEY)
-        return cls.BATTLEGROUND_ORDER[int(current) % len(cls.BATTLEGROUND_ORDER)]
+        last_time = redis.get(cls.LAST_UPDATE_TIME)
+        return cls.BATTLEGROUND_ORDER[int(current) % len(cls.BATTLEGROUND_ORDER)].append(last_time)
 
     @classmethod
     def set_info(cls, value):
         with redis.conn.pipeline() as pipe:
             with sessionCM() as session:
                 try:
-                    pipe.watch(cls.REDIS_KEY, cls.TIME_KEY)
+                    pipe.watch(cls.REDIS_KEY, cls.LAST_UPDATE_TIME)
                     pipe.multi()
                     redis.set(cls.REDIS_KEY, value)
                     time = Timer.now_time()
-                    redis.set(cls.TIME_KEY, time)
+                    redis.set(cls.LAST_UPDATE_TIME, time)
                     pipe.execute()
                     Jx3DailyRecord.create(session, update_time=time, info=json.dumps(cls.get_info()))
                 except WatchError:
@@ -43,20 +44,20 @@ class Jx3Info(object):
 
     @classmethod
     def auto_add_one(cls):
-        last_time = redis.get(cls.TIME_KEY)
+        last_time = redis.get(cls.LAST_UPDATE_TIME)
         if last_time and not Timer.is_in_yesterday(last_time):
             send_to_master("剑网三自动更新出错", "上次更新时间错误!")
             return False
         with redis.conn.pipeline() as pipe:
             with sessionCM() as session:
                 try:
-                    pipe.watch(cls.REDIS_KEY, cls.TIME_KEY)
+                    pipe.watch(cls.REDIS_KEY, cls.LAST_UPDATE_TIME)
                     pipe.multi()
                     res = redis.incr(cls.REDIS_KEY)
                     if not res:
                         raise RedisError
                     time = Timer.now_time()
-                    redis.set(cls.TIME_KEY, Timer.now_time())
+                    redis.set(cls.LAST_UPDATE_TIME, Timer.now_time())
                     pipe.execute()
                     current_jx3_info = json.dumps(cls.get_info())
                     Jx3DailyRecord.create(session, update_time=time, info=current_jx3_info)
--- lib/utils/time_utils.py
+++ lib/utils/time_utils.py
@@ -11,19 +11,15 @@ import datetime
 import pytz
 
 
-class Timer():
+def now_time():
+    return datetime.datetime.now(tz=pytz.timezone('Asia/Shanghai')).strftime("%Y-%m-%d %H-%M-%S")
 
-    @staticmethod
-    def now_time():
-        return datetime.datetime.now(tz=pytz.timezone('Asia/Shanghai')).strftime("%Y-%m-%d %H-%M-%S")
 
-    @staticmethod
-    def is_in_yesterday(time):
-        now = datetime.datetime.now(tz=pytz.timezone('Asia/Shanghai'))
-        oneday = datetime.timedelta(days=1)
-        yesterday = (now - oneday).strftime("%Y-%m-%d")
-        if time[:10] == yesterday[:10]:
-            return True
-        else:
-            return False
-
+def is_in_yesterday(time):
+    now = datetime.datetime.now(tz=pytz.timezone('Asia/Shanghai'))
+    oneday = datetime.timedelta(days=1)
+    yesterday = (now - oneday).strftime("%Y-%m-%d")
+    if time[:10] == yesterday[:10]:
+        return True
+    else:
+        return False
